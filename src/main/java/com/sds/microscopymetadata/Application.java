3ccd5b59b83870ad56bb3e52ba98c52892a7c102
Aleksander Sidorenko <aleksersidorenko@gmail.com>
2019-06-24 21:31:33 +0300

    fixes in Dockerfile

--- src/main/java/com/sds/microscopymetadata/Application.java
+++ src/main/java/com/sds/microscopymetadata/Application.java
@@ -1,4 +1,4 @@
-package com.sds.chemicalproperties;
+package com.sds.microscopymetadata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
