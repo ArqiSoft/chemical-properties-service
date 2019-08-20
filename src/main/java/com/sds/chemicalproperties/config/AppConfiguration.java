7e32b5a408b7d1cfaca5249eafb7dd0b78c199b1
aleksander_sidorenko <aleksander.sidorenko@ormdigital.com>
2019-08-21 00:25:26 +0300

    LEAN-69

--- src/main/java/com/sds/chemicalproperties/config/AppConfiguration.java
+++ src/main/java/com/sds/chemicalproperties/config/AppConfiguration.java
@@ -1,5 +1,6 @@
 package com.sds.chemicalproperties.config;
 
+import com.mongodb.ConnectionString;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -24,8 +25,7 @@ import com.npspot.jtransitlight.publisher.IBusControl;
 import com.npspot.jtransitlight.transport.JTransitLightTransportException;
 import com.sds.storage.BlobStorage;
 import com.sds.storage.gridfs.GridFSBlobStorage;
-// import com.sds.validation.JsonSchemaValidator;
-
+import org.apache.commons.lang3.StringUtils;
 
 @Configuration
 @EnableAutoConfiguration
@@ -38,14 +38,22 @@ public class AppConfiguration  {
 
     @Bean
     BlobStorage getBlobStorage(
-            @Value("${spring.data.mongodb.uri}") String mongoConnectionString, 
-            @Value("${spring.data.mongodb.grid-fs-database}") String dbName) {
+            @Value("${spring.data.mongodb.uri}") String mongoConnectionString) {
         
-        LOGGER.info("Connecting to MongoDB using url {}", mongoConnectionString);
-        LOGGER.info("MongoDB database name: {}", dbName);
-        
+        ConnectionString cs = new ConnectionString(mongoConnectionString);
+        if(cs.getPassword() != null)
+        {
+            String secureConnectionString = cs.toString().replaceFirst(":" + String.valueOf(cs.getPassword()) + "@", ":" + StringUtils.repeat("*", cs.getPassword().length) + "@")
+                    .replaceFirst("//" + cs.getUsername() + ":", "//" + StringUtils.repeat("*", cs.getUsername().length()) + ":");
+            LOGGER.info("Connecting to MongoDB using url {}", secureConnectionString); 
+        }
+        else{
+            LOGGER.info("Connecting to MongoDB using url {}", mongoConnectionString);
+        }
+       
+        LOGGER.info("MongoDB database name: {}", cs.getDatabase());
         return new GridFSBlobStorage(new MongoClient(
-                new MongoClientURI(mongoConnectionString)).getDatabase(dbName));    
+                new MongoClientURI(mongoConnectionString)).getDatabase(cs.getDatabase()));    
     }
     
     @Bean
